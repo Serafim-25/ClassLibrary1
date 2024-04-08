60e5d730092888f76576ad4a825c675476c2c6c0
Серафим Загородний <zagorodniy.serafim@mail.ru>
2024-04-08 22:19:38 +0300

    hkjh

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -9,7 +9,6 @@ namespace ClassLibrary1
     public class Class1
     {
         string x = "sdjvljfnv";
-        string x = "sdjvljfnv";
         string y = "Help   me";
 
         string z = "Hello";
