c927a1bfe4b7d1faf1ed12ce465fab018e898a76
Арсений Байдин <Arseniy2018@yandex.ru>
2024-04-11 14:51:28 +0300

    Updated RoomPlacer_V2

--- ClassLibrary1/RoomPlacerTest.cs
+++ ClassLibrary1/RoomPlacerTest.cs
@@ -1,9 +1,13 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.Exceptions;
 using Autodesk.Revit.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.ExceptionServices;
 
 namespace ClassLibrary1
 {
@@ -15,36 +19,63 @@ namespace ClassLibrary1
             UIDocument uIDocument = commandData.Application.ActiveUIDocument;
             Document document = uIDocument.Document;
 
-            //для теста берем любую 1 стену(даллее предполагаем, что эта стена - прямая)
-            Wall wall = new FilteredElementCollector(document).OfClass(typeof(Wall)).Cast<Wall>().First();
-
-            LocationCurve wallLine = wall.Location as LocationCurve;
-            Curve wallCurve = wallLine.Curve;
-            Line wline = wallLine.Curve as Line;
-            XYZ directionLine = wline.Direction;
-            UV vector = new UV(directionLine.Y, directionLine.X);
-            IList<XYZ> wallPoints = wallCurve.Tessellate();
-            
-
-            XYZ point1 = wallPoints[0];
-            XYZ point2 = wallPoints[1];
-            UV uvRoom1 = new UV(((point1.X+point2.X)/2),((point1.Y+point2.Y/2)));
-            double width = wall.Width;
-            uvRoom1 = uvRoom1 + width*vector;
+            Level roomLevel = new FilteredElementCollector(document).OfClass(typeof(Level)).FirstElement() as Level;
+            IEnumerable<Element> walls = new FilteredElementCollector(document).OfClass(typeof(Wall)).ToElements();
 
 
-            Level levelRoom1 = new FilteredElementCollector(document).OfClass(typeof(Level)).FirstElement() as Level;
-
-            using (Transaction transaction = new Transaction(document))
+            // предполагаем, что эта стена - прямая
+            foreach (var wallt in walls)
             {
-                transaction.Start("Safety transaction");
-                Room room = document.Create.NewRoom(levelRoom1, uvRoom1);
-                if (null == room)
+                Wall wall = wallt as Wall;
+                Line wline = (wall.Location as LocationCurve).Curve as Line;
+                XYZ directionLine = wline.Direction;
+                UV vector = new UV(directionLine.Y, directionLine.X);
+
+                Curve wallCurve = (wall.Location as LocationCurve).Curve;
+                double width = wall.Width;
+
+                for (int i = 0; i < wallCurve.Length; i++)
                 {
-                    transaction.Commit();
-                    return Result.Failed;
+                    UV currentWallPoint = new UV(wallCurve.Evaluate(i, false)[0], wallCurve.Evaluate(i, false)[1]);
+
+                    UV roomPos1 = currentWallPoint + 0.5 * width * vector;
+                    UV roomPos2 = currentWallPoint - 0.5 * width * vector;
+
+
+                    using (Transaction transaction = new Transaction(document))
+                    {
+                        transaction.Start("Safety transaction");
+
+                        Room room1 = document.Create.NewRoom(roomLevel, roomPos1);
+                        ElementId room1Id = room1.Id;
+                        try
+                        {
+                            if (room1.Area == 0)
+                            {
+                                throw new Exception();
+                            }
+                        }
+                        catch
+                        {
+                            document.Delete(room1Id);
+                        }
+                        Room room2 = document.Create.NewRoom(roomLevel, roomPos2);
+                        ElementId room2Id = room2.Id;
+                        try
+                        {
+                            if (room2.Area == 0)
+                            {
+                                throw new Exception();
+                            }
+                        }
+                        catch
+                        {
+                            document.Delete(room2Id);
+                        }
+
+                        transaction.Commit();
+                    }
                 }
-                transaction.Commit();
             }
             return Result.Succeeded;
         }
