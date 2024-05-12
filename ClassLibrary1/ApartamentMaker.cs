1c1450515fefac137db0996c0b8129547536ecf7
Арсений Байдин <Arseniy2018@yandex.ru>
2024-05-12 22:04:52 +0300

    ApartamentMaker__V2

--- ClassLibrary1/ApartamentMaker.cs
+++ ClassLibrary1/ApartamentMaker.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
+using Pol = ClassLibrary2;
 
 namespace ClassLibrary1
 {
@@ -20,78 +18,135 @@ namespace ClassLibrary1
             Document document = uIDocument.Document;
 
             List<Level> level = new FilteredElementCollector(document).OfClass(typeof(Level)).OfCategory(BuiltInCategory.OST_Levels).Cast<Level>().ToList();
-            //TaskDialog.Show("Заголовок. Количество уровней", level.Count.ToString());
+            TaskDialog.Show("Заголовок", "Начало команды");
 
             Guid guidPlaceType = new Guid("15723a20-5ef0-40db-a947-2be11932e630"); //guid Кврт.ТипПомещения(5 тип - нежилое)
             Guid guidNumberApartment = new Guid("77f09e29-8873-4223-bdd9-4c9050cf18a4");// guid Кврт.НомерКвартиры
+
+            string nameApartmentPlaceType = "Кврт.ТипПомещения";
+            string nameApartmentNumber = "Кврт.НомерКвартиры";
+            string nameApartmentArea = "Кврт.ПлощадьКвартиры";
+            string nameApartmentAreaResidential = "Кврт.ПлощадьКвартирыЖилая";
+            string nameApartmentAreaGeneral = "Кврт.ПлощадьКвартирыОбщая";
+            string nameApartmentNumberRooms = "Кврт.ЧислоКомнат";
+            string nameApartmentRoomIndex = "Кврт.ИндексПомещения";
+
             int currentNumberApartment = 1;
             const int numberNonResidentialPremises = 5;
 
-                for (int j = 0; j < level.Count; j++)
+            
+            for (int j = 0; j < level.Count; j++)
+            {
+                Dictionary<string, UV> Apartments = new Dictionary<string, UV>(); // номер кв. и координата центра
+                Dictionary<string, Dictionary<UV, double>> ApartmentsPreliminary = new Dictionary<string, Dictionary<UV, double>>(); // номер кв. и коорд<->площадь
+                List<SpatialElement> rooms = new FilteredElementCollector(document).OfClass(typeof(SpatialElement)).Cast<SpatialElement>().Where(it => it.Level.Id.Equals(level[j].Id)).ToList();
+                List<FamilyInstance> windows = new FilteredElementCollector(document).OfClass(typeof(FamilyInstance)).OfCategory(BuiltInCategory.OST_Windows).Cast<FamilyInstance>().Where(it => it.LevelId.Equals(level[j].Id) && it.ToRoom != null && it.FromRoom != null).ToList();
+                List<FamilyInstance> doors = new FilteredElementCollector(document).OfClass(typeof(FamilyInstance)).OfCategory(BuiltInCategory.OST_Doors).Cast<FamilyInstance>().Where(it => it.LevelId.Equals(level[j].Id) && it.ToRoom != null && it.FromRoom != null).ToList();
+                List<FamilyInstance> opening = new List<FamilyInstance>();
+                Dictionary<ElementId, Parameter[]> roomsDict = new Dictionary<ElementId, Parameter[]>();
+                using (Transaction transaction = new Transaction(document))
                 {
-                    using (Transaction transaction = new Transaction(document))
+                    transaction.Start("Safety transaction");
+                    opening.AddRange(windows);
+                    opening.AddRange(doors);
+ 
+                    for (int i = 0; i < rooms.Count; i++)
                     {
-                        transaction.Start("Safety transaction");
-                        List<FamilyInstance> windows = new FilteredElementCollector(document).OfClass(typeof(FamilyInstance)).OfCategory(BuiltInCategory.OST_Windows).Cast<FamilyInstance>().Where(it => it.LevelId.Equals(level[j].Id) && it.ToRoom != null && it.FromRoom != null).ToList();
-                        List<FamilyInstance> doors = new FilteredElementCollector(document).OfClass(typeof(FamilyInstance)).OfCategory(BuiltInCategory.OST_Doors).Cast<FamilyInstance>().Where(it => it.LevelId.Equals(level[j].Id) && it.ToRoom != null && it.FromRoom != null).ToList();
-                        List<FamilyInstance> opening = new List<FamilyInstance>();
-                        opening.AddRange(windows);
-                        opening.AddRange(doors);
-                        List<SpatialElement> rooms = new FilteredElementCollector(document).OfClass(typeof(SpatialElement)).Cast<SpatialElement>().Where(it => it.Level.Id.Equals(level[j].Id)).ToList();
-                        Dictionary<ElementId, Parameter[]> roomsDict = new Dictionary<ElementId, Parameter[]>();
-
-                        for (int i = 0; i < rooms.Count; i++)
+                        ParameterSet parameterSet = rooms[i].Parameters;
+                        Parameter roomType = null;
+                        Parameter apartmentNumber = null;
+                        foreach (Parameter para in parameterSet)
                         {
-                            ParameterSet parameterSet = rooms[i].Parameters;
-                            Parameter roomType = null;
-                            Parameter apartmentNumber = null;
-                            foreach (Parameter para in parameterSet)
-                            {
-                                if (para.IsShared && para.GUID == guidNumberApartment) { apartmentNumber = para; }
-                                else if (para.IsShared && para.GUID == guidPlaceType) { roomType = para; }
-                            }
-                            roomsDict.Add(rooms[i].Id, new Parameter[] { roomType, apartmentNumber });
+                            if (para.IsShared && para.Definition.Name == nameApartmentNumber) { apartmentNumber = para; }
+                            else if (para.IsShared && para.Definition.Name == nameApartmentPlaceType) { roomType = para; }
                         }
-                        try
+                        roomsDict.Add(rooms[i].Id, new Parameter[] { roomType, apartmentNumber });
+                    }
+                    try
+                    {
+
+                        for (int u = 0; u < rooms.Count; u++)
                         {
-
-                            for (int u = 0; u < rooms.Count; u++)
+                            if (roomsDict[rooms[u].Id][0].AsInteger() != numberNonResidentialPremises && roomsDict[rooms[u].Id][1].AsString() == string.Empty)
                             {
-                                if (roomsDict[rooms[u].Id][0].AsInteger() != numberNonResidentialPremises && roomsDict[rooms[u].Id][1].AsString() == String.Empty)
-                                {
-                                    roomsDict[rooms[u].Id][1].Set("Кв." + currentNumberApartment.ToString());
+                                roomsDict[rooms[u].Id][1].Set("Кв." + currentNumberApartment.ToString());
 
-                                    for (int t = 0; t < opening.Count; t++)
+                                for (int t = 0; t < opening.Count; t++)
+                                {
+                                    if (roomsDict[opening[t].ToRoom.Id][1].AsString() == "Кв." + currentNumberApartment.ToString())
                                     {
-                                        if (roomsDict[opening[t].ToRoom.Id][1].AsString() == "Кв." + currentNumberApartment.ToString())
+                                        if (roomsDict[opening[t].FromRoom.Id][0].AsInteger() != numberNonResidentialPremises && (roomsDict[opening[t].FromRoom.Id][1].AsString() != "Кв." + currentNumberApartment.ToString()))
                                         {
-                                            if (roomsDict[opening[t].FromRoom.Id][0].AsInteger() != numberNonResidentialPremises && (roomsDict[opening[t].FromRoom.Id][1].AsString() != "Кв." + currentNumberApartment.ToString()))
-                                            {
-                                                roomsDict[opening[t].FromRoom.Id][1].Set("Кв." + currentNumberApartment.ToString());
-                                                t = 0;
-                                            }
-                                        }
-                                        else if (roomsDict[opening[t].FromRoom.Id][1].AsString() == "Кв." + currentNumberApartment.ToString())
-                                        {
-                                            if (roomsDict[opening[t].ToRoom.Id][0].AsInteger() != numberNonResidentialPremises && (roomsDict[opening[t].ToRoom.Id][1].AsString() != "Кв." + currentNumberApartment.ToString()))
-                                            {
-                                                roomsDict[opening[t].ToRoom.Id][1].Set("Кв." + currentNumberApartment.ToString());
-                                                t = 0;
-                                            }
+                                            roomsDict[opening[t].FromRoom.Id][1].Set("Кв." + currentNumberApartment.ToString());
+                                            t = 0;
                                         }
                                     }
-                                    currentNumberApartment += 1;
+                                    else if (roomsDict[opening[t].FromRoom.Id][1].AsString() == "Кв." + currentNumberApartment.ToString())
+                                    {
+                                        if (roomsDict[opening[t].ToRoom.Id][0].AsInteger() != numberNonResidentialPremises && (roomsDict[opening[t].ToRoom.Id][1].AsString() != "Кв." + currentNumberApartment.ToString()))
+                                        {
+                                            roomsDict[opening[t].ToRoom.Id][1].Set("Кв." + currentNumberApartment.ToString());
+                                            t = 0;
+                                        }
+                                    }
                                 }
+                                currentNumberApartment += 1;
                             }
                         }
-                        catch (Exception e)
-                        {
-                            TaskDialog.Show("Исключение", e.Message);
-                            continue;
-                        }
-                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        TaskDialog.Show("Исключение", e.Message);
+                        continue;
+                    }
+                    transaction.Commit();
+                }
+                //TaskDialog.Show("Заголовок", "Конец транзакции");
+                foreach (var roomm in rooms)
+                {
+                    Room room = roomm as Room;
+                    double area = room.Area;
+                    IEnumerator<GeometryObject> geometryEl = room.ClosedShell.GetEnumerator();
+                    geometryEl.MoveNext();
+                    Solid solid = geometryEl.Current as Solid;
+                    UV center = UV.Zero;
+                    if (solid != null && roomsDict[room.Id][1].AsString() != null && roomsDict[room.Id][0].AsInteger() != numberNonResidentialPremises)
+                    {
+                        center = new UV(solid.ComputeCentroid().X, solid.ComputeCentroid().Y);
+                    }
+                    else continue;
+                    if (!ApartmentsPreliminary.ContainsKey(roomsDict[room.Id][1].AsString()))
+                    {
+                        Dictionary<UV, double> keyValuePairs = new Dictionary<UV, double> { { center, room.Area } };
+                        ApartmentsPreliminary.Add(roomsDict[room.Id][1].AsString(), keyValuePairs);
+                    }
+                    else
+                    {
+                        ApartmentsPreliminary[roomsDict[room.Id][1].AsString()].Add(center, area);
                     }
                 }
+                foreach (var apart in ApartmentsPreliminary)
+                {
+                    UV numerator = new UV(0, 0);
+                    double denominator = 0;
+                    foreach (var room in apart.Value)
+                    {
+                        numerator += room.Key * room.Value;
+                        denominator += room.Value;
+                    }
+                    UV center = numerator / denominator;
+                    Apartments.Add(apart.Key, center);
+                }
+                Pol.Point[] points = new Pol.Point[Apartments.Count];
+                var apartEnumerator = Apartments.GetEnumerator();
+                for (int i = 0; i < points.Length; i++)
+                {
+                    apartEnumerator.MoveNext();
+                    var apart = apartEnumerator.Current;
+                    points[i] = new Pol.Point(apart.Value.U, apart.Value.V);
+                }
+                Pol.Point[] orderedPoints = Pol.PolygonCreator.Assembly(points);
+            }
             return Result.Succeeded;
         }
     }
---
+++ ClassLibrary1/PolygonCreator.cs
@@ -0,0 +1,293 @@
+﻿using System.Collections.Generic;
+using System;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace ClassLibrary2
+{
+    public static class PolygonCreator
+    {
+        static Random random = new Random();
+        static Random random2 = new Random();
+        static public Point[] Assembly(Point[] points)
+        {
+            Polygon myPolygon = new Polygon(points);
+            shufflePolygon(ref myPolygon);
+            randomTestPolygon(ref myPolygon, 250);
+            Point[] res = new Point[points.Length];
+            for (int i = 0; i < points.Length; i++)
+            {
+                res[i] = myPolygon.Edges[i].P1;
+            }
+            return res;
+        }
+        public static void randomTestPolygon(ref Polygon p, int t = 20)
+        {
+            int hasbeenbestfor = 0;
+            double bestscore = p.Perimeter();
+            Polygon pbest = p.Clone() as Polygon;
+            while (hasbeenbestfor < t)
+            {
+                shufflePolygon(ref p);
+                unWindPolygon(ref p);
+                double currentScore = p.Perimeter();
+                //Console.WriteLine("{0} {1} {2}", hasbeenbestfor, bestscore, currentScore);
+                if (currentScore < bestscore)
+                {
+                    bestscore = currentScore;
+                    pbest = p.Clone() as Polygon;
+                    hasbeenbestfor = 0;
+                }
+                else
+                {
+                    hasbeenbestfor++;
+                }
+                p.Vertices = pbest.Vertices;
+                p.Edges = pbest.Edges;
+            }
+        }
+        public static bool isInInterval(double x0, double x1, double x2)
+        {
+            double eps = Math.Pow(10, -5);
+            if (((x1 + eps) < x0 && x0 < (x2 - eps)) || ((x2 + eps) < x0 && x0 < (x1 - eps)))
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static bool isIntersect(Line line1, Line line2)
+        {
+            Line l1 = line1.Clone() as Line;
+            Line l2 = line2.Clone() as Line;
+            if (l1.Length == 0 || l2.Length == 0) return false;
+            while ((l1.P1.X == l1.P2.X) || (l2.P1.X == l2.P2.X))
+            {
+                l1.rotate(45 * Math.PI / 180);
+                l2.rotate(45 * Math.PI / 180);
+            }
+            double a1 = (l1.P2.Y - l1.P1.Y) / (l1.P2.X - l1.P1.X);
+            double a2 = (l2.P2.Y - l2.P1.Y) / (l2.P2.X - l2.P1.X);
+            double b1 = l1.P1.Y - a1 * l1.P1.X;
+            double b2 = l2.P1.Y - a2 * l2.P1.X;
+            if (a1 == a2)
+            {
+                if (b1 == b2)
+                {
+                    if (isInInterval(l2.P1.X, l1.P1.X, l1.P2.X) ||
+                        isInInterval(l2.P2.X, l1.P1.X, l1.P2.X) ||
+                        isInInterval(l1.P1.X, l2.P1.X, l2.P2.X) ||
+                        isInInterval(l1.P2.X, l2.P1.X, l2.P2.X))
+                    {
+                        return true;
+                    }
+                    else return false;
+                }
+                else return false;
+            }
+            else
+            {
+                double x = -(b2 - b1) / (a2 - a1);
+                if (isInInterval(x, l1.P1.X, l1.P2.X) &&
+                    isInInterval(x, l2.P1.X, l2.P2.X))
+                {
+                    return true;
+                }
+                else return false;
+            }
+        }
+        public static bool anyIntersect(Polygon p)
+        {
+            bool[,] bools = new bool[p.N, p.N];
+            for (int j = 0; j < p.N; j++)
+            {
+                for (int k = 0; k < p.N; k++)
+                {
+                    bools[j, k] = isIntersect(p.Edges[j], p.Edges[k]);
+                }
+            }
+            foreach (var h in bools)
+            {
+                if (h == true) return true;
+            }
+            return false;
+        }
+        public static void unWindPolygon(ref Polygon p, int bailout = 100)
+        {
+            int t = 0;
+            while (anyIntersect(p) && t < bailout)
+            {
+                t += 1;
+                bool breakingFlag = false;
+                for (int j = 0; j < p.N; j++)
+                {
+                    for (int k = 0; k < p.N; k++)
+                    {
+                        if (isIntersect(p.Edges[j], p.Edges[k]))
+                        {
+                            Point p12;
+                            Point p22;
+                            if (j < p.N - 1)
+                            {
+                                p12 = p.Vertices[j + 1];
+                            }
+                            else { p12 = p.Vertices[0]; }
+                            if (k < p.N - 1)
+                            {
+                                p22 = p.Vertices[k + 1];
+                                p.Vertices[k + 1] = p12;
+                            }
+                            else
+                            {
+                                p22 = p.Vertices[0];
+                                p.Vertices[0] = p12;
+                            }
+                            if (j < p.N - 1) { p.Vertices[j + 1] = p22; }
+                            else { p.Vertices[0] = p22; }
+                            breakingFlag = true;
+                            break;
+                        }
+                    }
+                    if (breakingFlag) break;
+                }
+                p.Edges = new Polygon(p.Vertices).Edges;
+            }
+        }
+        public static void shufflePolygon(ref Polygon p)
+        {
+            for (int i = p.Vertices.Length - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+                // обменять значения data[j] и data[i]
+                var temp = p.Vertices[j];
+                (p.Vertices[j], p.Vertices[i]) = (p.Vertices[i], temp);
+            }
+            p.Edges = new Polygon(p.Vertices).Edges;
+        }
+    }
+    public class Point : ICloneable
+    {
+        private double _X, _Y;
+        public Point(double x, double y)
+        {
+            _X = x;
+            _Y = y;
+        }
+        public double X
+        {
+            get { return _X; }
+        }
+        public double Y
+        {
+            get { return _Y; }
+        }
+
+        public object Clone()
+        {
+            return new Point(X, Y);
+        }
+
+        internal void rotate(double theta)
+        {
+
+            Matrix<double> A = DenseMatrix.OfArray(new double[,] {
+                                {Math.Cos(theta),-Math.Sin(theta)},
+                                {Math.Sin(theta),Math.Cos(theta)}});
+            Vector<double> B = CreateVector.DenseOfArray(new double[] { X, Y });
+            _X = A.Multiply(B)[0];
+            _Y = A.Multiply(B)[1];
+        }
+    }
+    public class Line : ICloneable
+    {
+        private Point _P1, _P2;
+        public Line(Point p1, Point p2)
+        {
+            _P1 = p1;
+            _P2 = p2;
+        }
+        public Point P1
+        {
+            get { return _P1; }
+        }
+        public Point P2
+        {
+            get { return _P2; }
+        }
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow((P1.X - P2.X), 2) + Math.Pow((P1.Y - P2.Y), 2));
+            }
+        }
+
+        public object Clone()
+        {
+            return new Line(P1.Clone() as Point, P2.Clone() as Point);
+        }
+
+
+
+        public void rotate(double theta)
+        {
+            _P1.rotate(theta);
+            _P2.rotate(theta);
+        }
+    }
+    public class Polygon : ICloneable
+    {
+        private int _N;
+        private double _Perimeter;
+        public int N
+        {
+            get { return _N; }
+        }
+        public Point[] Vertices;
+        public List<Line> Edges = new List<Line>();
+
+        public double Perimeter()
+        {
+            double per = 0;
+            for (int i = 0; i < Edges.Count; i++)
+            {
+                per += Edges[i].Length;
+            }
+            _Perimeter = per;
+            return per;
+        }
+        public Polygon(Point[] points)
+        {
+            _N = points.Length;
+            Vertices = points;
+            int k = 0;
+            for (; k < N - 1; k++)
+            {
+                Edges.Add(new Line(points[k], points[k + 1]));
+            }
+            Edges.Add(new Line(points[N - 1], points[0]));
+            double per = 0;
+            for (int i = 0; i < Edges.Count; i++)
+            {
+                per += Edges[i].Length;
+            }
+            _Perimeter = per;
+        }
+        public void rotate(double theta)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                Vertices[i].rotate(theta);
+                Edges[i].rotate(theta);
+            }
+        }
+
+        public object Clone() => new Polygon(Vertices);
+        public void Print()
+        {
+            for (int i = 0; i < Edges.Count; i++)
+            {
+                Console.WriteLine("({0} {1})", Edges[i].P1.X, Edges[i].P1.Y);
+            }
+        }
+    }
+}
